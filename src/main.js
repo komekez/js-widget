9b6e51e981e94966596bc0a5f965e32b6d28cb83
kaus2595 <kausr2595@gmail.com>
2021-07-21 01:10:35 +0530

    Package JSON Added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import { ping } from "./services";
+import { show } from "./views/messages";
+
+const supportedAPI = ["init", "messages"];
+
+function app(window) {
+    console.log("JS-Widget Starting");
+}
+
+
+app(window);
